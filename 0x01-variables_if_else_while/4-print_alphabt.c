0581dbbb41edabc63d3c5fdac1ba607e0bdd3b09
Edah Azariah <edahaz2810@gmail.com>
2022-09-13 09:52:53 -0700

    solution to task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/** main- prints alphabets from a-z
+ * excluding e and q
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
