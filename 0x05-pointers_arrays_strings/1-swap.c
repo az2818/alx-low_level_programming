6ff9fa538ab97d53de5a52a393aace2cb4bde89f
Edah Azariah <edahaz2810@gmail.com>
2022-09-20 10:23:37 -0700

    solution to task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#inclued "main.h"
+
+/**
+ * main- function that swaps the value of two integers
+ * @a- pointer 1
+ * @b- pointer 2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
